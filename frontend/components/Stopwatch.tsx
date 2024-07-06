6148a990dd3afd75389de56c2d81398f0016355d
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-07 08:38:20 +0900

    update

--- frontend/components/Stopwatch.tsx
+++ frontend/components/Stopwatch.tsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 
 const Stopwatch = () => {
-  const [time, setTime] = useState(0.0);
+  const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    let interval: number | undefined;
+    let interval: ReturnType<typeof setInterval> | undefined;
     if (isRunning) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 1);
@@ -18,6 +18,18 @@ const Stopwatch = () => {
     }
     return () => clearInterval(interval);
   }, [isRunning]);
+
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
 
   const startStopwatch = () => {
     setIsRunning(true);
@@ -35,16 +47,21 @@ const Stopwatch = () => {
     }
   };
 
+  const formatTime = (time: number) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    const padTime = (unit: number) => String(unit).padStart(2, '0');
+    return `${padTime(hours)}h /${padTime(minutes)}m /${padTime(seconds)}s`;
+  };
+
   return (
     <div className={'text-4xl flex flex-col space-y-3'}>
-      <div>
-        <div>{time}s</div>
-      </div>
-      <div className={'flex justify-center space-x-4 '}>
+      <div>{formatTime(time)}</div>
+      <div className={'flex justify-center space-x-4'}>
         <Button onClick={startStopwatch} variant={'default'} disabled={isRunning}>
           Start/Restart
         </Button>
-
         <Button onClick={stopStopwatch} variant={'yellow'} disabled={!isRunning}>
           Stop
         </Button>
