910d573a4718f1ccf8c8fa0fef99e898e7c2bd68
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-09 05:41:21 +0900

    add localStorage feature so that the form data does not get lost after reloading

--- study-time-tracker-frontend/app/studyTimeForm/page.tsx
+++ study-time-tracker-frontend/app/studyTimeForm/page.tsx
@@ -12,21 +12,53 @@ import StudyTimeField from '@/components/formField/StudyTimeField';
 import { formSchema } from '@/schemas/formSchema';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const StudyTimeFormPage = () => {
   const router = useRouter();
   const { user, error, isLoading } = useUser();
 
+  // localStorageからフォームデータを読み込む関数
+  const loadFormData = () => {
+    const savedData = localStorage.getItem('studyTimeForm');
+    return savedData ? JSON.parse(savedData) : {};
+  };
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      subject: '',
-      studyTime: '',
+      ...loadFormData(),
       username: user?.name,
       email: user?.email,
     },
   });
+
+  // formのデータをlocalStorageに保存するための関数
+  const saveFormData = (data: z.infer<typeof formSchema>) => {
+    localStorage.setItem('studyTimeForm', JSON.stringify(data));
+  };
+
+  // formの値を監視して、formの値が変更されるたびにlocalStorageに保存する
+  //　componentがunmountされるとき、または依存関係が変更されるときに監視を解除する
+  useEffect(() => {
+    const subscription = form.watch((value) => {
+      saveFormData(value);
+    });
+    return () => subscription.unsubscribe();
+  }, [form]);
+
+  // show warning before a reload happens.
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -34,6 +66,7 @@ const StudyTimeFormPage = () => {
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
+    localStorage.removeItem('studyTimeForm');
     router.push('/success');
   }
 
